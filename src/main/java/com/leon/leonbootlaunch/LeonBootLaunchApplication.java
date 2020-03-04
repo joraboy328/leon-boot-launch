62411d5c41e54040c751f367b7985d118809844e
leon <gang.liu@dic.com.cn>
2020-03-04 08:53:06 +0800

    init commit

---
+++ src/main/java/com/leon/leonbootlaunch/LeonBootLaunchApplication.java
@@ -0,0 +1,13 @@
+package com.leon.leonbootlaunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonBootLaunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeonBootLaunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leon/leonbootlaunch/LeonBootLaunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.leonbootlaunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonBootLaunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
